0d83c2804581e0057ea17b44b7788226021f965a
Charlie Mitchell <charlie.mitchell@mail.utoronot.ca>
2019-11-26 05:47:06 -0500

    Update settings.py

--- website/website/settings.py
+++ website/website/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'dd^wa*3hwzkw+@ip@gqc*6lq557&i=q1ui@b&or7h3jo(-i4=='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['http://ec2-3-16-181-13.us-east-2.compute.amazonaws.com:8000/']
+ALLOWED_HOSTS = []
 
 
 # Application definition
