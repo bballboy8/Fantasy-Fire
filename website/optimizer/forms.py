cd565bec39c83984a758ff434b81938ce0812289
Charlie Mitchell <charlie.mitchell@mail.utoronot.ca>
2019-11-26 05:40:50 -0500

    Update forms.py

--- website/optimizer/forms.py
+++ website/optimizer/forms.py
@@ -7,7 +7,7 @@ CHOICES=[('not_random','Not Random'),
          ('gaussian', 'Gaussian')]
 
 def get_slates():
-    slate_files = os.listdir(Path(r'//home/ubuntu/Fantasy-Fire/website/optimizer/Slates')
+    slate_files = os.listdir(Path(r'//home/ubuntu/Fantasy-Fire/website/optimizer/Slates'))
     slates = []
     for slate in slate_files:
         if "Main" in slate:
