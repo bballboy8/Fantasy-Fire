d330fcc6ee5ca952a0a95c0b66448df6f3a1dc5a
Charlie Mitchell <charlie.mitchell@mail.utoronot.ca>
2019-12-09 18:48:23 -0500

    Update views.py

--- website/optimizer/views.py
+++ website/optimizer/views.py
@@ -69,7 +69,7 @@ def create_optimizer(request):
                 inplace=True)
             html_table = df.to_html(index=False, justify='left', escape=False, table_id='slateData',
                                     classes=[
-                                        'table w3-table-all table-bordered table-striped table-hover table-responsive table-sm searchable sortable tablesorter tablesorter-default, container-fluid''])
+                                        'table w3-table-all table-bordered table-striped table-hover table-responsive table-sm searchable sortable tablesorter tablesorter-default, container-fluid'])
             return render(request, 'optimizer/optimizer.html',
                           {'form': form, 'player_table': html_table, 'form2': form2, 'slate': slate})
         if form.is_valid():
