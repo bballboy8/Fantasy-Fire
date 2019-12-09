d4e1ab88417f3cfb267d5098f385eff57ab68d1c
Charlie Mitchell <charlie.mitchell@mail.utoronot.ca>
2019-12-09 07:41:13 -0500

    Update Optimizer.py

--- website/optimizer/Optimizer.py
+++ website/optimizer/Optimizer.py
@@ -16,7 +16,7 @@ def create_predictions(predictions,
     np.set_printoptions(threshold=10000000)
     df = pd.read_csv(slate)
     df.insert(7, "Opp", '', True)
-    df["Opp"][df['Game Info'].str[:3].contains(df['TeamAbbrev'])] = df['Game Info'].str[4:7]
+    df["Opp"][df['Game Info'].str[:3].str.contains(df['TeamAbbrev'])] = df['Game Info'].str[4:7]
     df["Opp"][df['Opp'].empty] = df['TeamAbbrev']
     # df['Opp'] = np.where(df['TeamAbbrev'].str != df['Game Info'].str[:3]), df['Game Info'].str[:3], df['Game Info'].str[4:7])
     # df['Opp'] = df['Game Info'].str[:3] if df['TeamAbbrev'].str != df['Game Info'].str[:3] else df['Game Info'].str[4:7]
