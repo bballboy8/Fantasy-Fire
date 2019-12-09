07f59ce1effff70ce1ec414f647056f5df1cc054
Charlie Mitchell <charlie.mitchell@mail.utoronot.ca>
2019-12-09 17:37:54 -0500

    Update views.py

--- website/optimizer/views.py
+++ website/optimizer/views.py
@@ -69,7 +69,7 @@ def create_optimizer(request):
                 inplace=True)
             html_table = df.to_html(index=False, justify='left', escape=False, table_id='slateData',
                                     classes=[
-                                        'table sorted table-bordered table-striped table-hover table-responsive table-sm container-fluid sortable'])
+                                        'table w3-table-all table-bordered table-striped table-hover table-responsive table-sm searchable sortable tablesorter tablesorter-default, container-fluid''])
             return render(request, 'optimizer/optimizer.html',
                           {'form': form, 'player_table': html_table, 'form2': form2, 'slate': slate})
         if form.is_valid():
@@ -167,7 +167,7 @@ def create_optimizer(request):
         # df['Value'] = df['Predicted_FP'] / (df['Salary'] / 1000)
         html_table = df.to_html(index=False, justify='left', escape=False, table_id='slateData',
                                 classes=[
-                                    'table sorted table-bordered table-striped table-hover table-responsive table-sm container-fluid sortable'])
+                                    'table w3-table-all table-bordered table-striped table-hover table-responsive table-sm searchable sortable tablesorter tablesorter-default, container-fluid''])
                                     # 'table table-bordered table-striped'])
         return render(request, 'optimizer/optimizer.html',
                       {'form': form, 'player_table': html_table, 'form2': form2, 'slate': 'Main_Slate'})
